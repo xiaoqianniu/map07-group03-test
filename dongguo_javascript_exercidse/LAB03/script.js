206b12968bf9d2116494c17b46353383b8747e37
Dongguo WU <david.dong.guo@gmail.com>
2023-03-29 12:13:27 -0400

    delete global variables

--- dongguo_javascript_exercidse/LAB03/script.js
+++ dongguo_javascript_exercidse/LAB03/script.js
@@ -1,7 +1,5 @@
 function submitClicked() {
   var age = document.getElementById('myNumber').value
-  console.log('the name is ')
-  console.log('the age is : ' + age)
   document.getElementById('msg').innerHTML = 'Your age is: ' + age
 }
 function numberChanging() {
