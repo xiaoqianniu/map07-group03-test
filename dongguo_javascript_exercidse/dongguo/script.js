edff4c4d8fb9e07cc9bfac7f074f9c22e6f92f10
Dongguo WU <david.dong.guo@gmail.com>
2023-03-29 17:05:34 -0400

    update user flow

--- dongguo_javascript_exercidse/dongguo/script.js
+++ dongguo_javascript_exercidse/dongguo/script.js
@@ -11,6 +11,18 @@ function numberChanged() {
   console.log('age :' + age)
 }
 
+function addClicked() {
+  var n1 = document.getElementById('number1').value
+  var n2 = document.getElementById('number2').value
+  document.getElementById('result').innerHTML = Number(n1) + Number(n2)
+}
+
+function subtractClicked() {
+  var n1 = document.getElementById('number1').value
+  var n2 = document.getElementById('number2').value
+  document.getElementById('result').innerHTML = n1 - n2
+}
+
 function multiplyClicked() {
   var n1 = document.getElementById('number1').value
   var n2 = document.getElementById('number2').value
@@ -20,5 +32,9 @@ function multiplyClicked() {
 function divideClicked() {
   var n1 = document.getElementById('number1').value
   var n2 = document.getElementById('number2').value
+  if (n2 === '0') {
+    document.getElementById('result').innerHTML = '0 can not be divided'
+    return
+  }
   document.getElementById('result').innerHTML = n1 / n2
 }
