b1a382033cae3c44dcc32741477d424435344366
Dongguo WU <david.dong.guo@gmail.com>
2023-04-26 20:49:48 -0400

    layout

---
+++ sushi-next-project/src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SearchBar from './SearchBar'
+import SushiList from './sushiList'
+
+function Main() {
+  return (
+    <div>
+      <SearchBar />
+      <SushiList />
+    </div>
+  )
+}
+
+export default Main
