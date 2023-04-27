2ad06159815a73e0455e58db5dc3c11956716f9b
Dongguo WU <david.dong.guo@gmail.com>
2023-04-26 20:48:25 -0400

    list

--- sushi-next-project/src/components/sushiList.js
+++ sushi-next-project/src/components/sushiList.js
@@ -1,126 +1,60 @@
-import React from 'react'
-import Image from 'next/image'
+import React, { useState, useEffect } from 'react'
+import SushiCard from './sushiCard'
+
+import PropTypes from 'prop-types'
 
 function SushiList() {
-  const array = [
-    {
-      id: 1,
-      title: 'California',
-      image: '/images/Sushi_1.jpg',
-      subtitle: 'Japan found itself in midst',
-      'card-text': 'the rice and fish reduced the time to about one month',
-      price: '$7',
-      button: 'Order a sushi',
-    },
-    {
-      id: 2,
-      title: 'Ca',
-      image: '/images/Sushi_1.jpg',
-      subtitle: 'Japan found itself in midst',
-      'card-text': 'the rice and fish reduced the time to about one month',
-      price: '$10',
-      button: 'Order a sushi',
-    },
-    {
-      id: 3,
-      title: 'Caornia',
-      image: '/images/Sushi_1.jpg',
-      subtitle: 'Japan found itself in midst',
-      'card-text': 'the rice and fish reduced the time to about one month',
-      price: '$15',
-      button: 'Order a sushi',
-    },
-    {
-      id: 4,
-      title: 'ornia',
-      image: '/images/Sushi_1.jpg',
-      subtitle: 'Japan found itself in midst',
-      'card-text': 'the rice and fish reduced the time to about one month',
-      price: '$12',
-      button: 'Order a sushi',
-    },
-    {
-      id: 5,
-      title: 'California',
-      image: '/images/Sushi_1.jpg',
-      subtitle: 'Japan found itself in midst',
-      'card-text': 'the rice and fish reduced the time to about one month',
-      price: '$13',
-      button: 'Order a sushi',
-    },
-    {
-      id: 6,
-      title: 'California',
-      image: '/images/Sushi_1.jpg',
-      subtitle: 'Japan found itself in midst',
-      'card-text': 'the rice and fish reduced the time to about one month',
-      price: '$8',
-      button: 'Order a sushi',
-    },
-  ]
+  const [list, setList] = useState([])
 
-  const list = array.map((item) => (
-    <div
-      id="card-one"
-      className="col-xs-12 col-sm-6 col-md-3 col-lg-3"
-      key={item.id}
-    >
-      <h4 className="card-title">{item.title}</h4>
-      <Image
-        src="/images/Sushi_1.jpg"
-        className="card-img-top"
-        alt="..."
-        width="300"
-        height="200"
-      />
-      <div className="card-body">
-        <h5 className="card-title">Japan found itself in midst</h5>
-        <p className="card-text">
-          the rice and fish reduced the time to about one month
-        </p>
-        <div className="card-price-btn">
-          <p id="card-price">$7</p>
-          <button className="sushi-btn">Order a Sushi</button>
-        </div>
-      </div>
-    </div>
-  ))
+  const serverUrl = '/api/sushi'
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(serverUrl)
+      const data = await response.json()
+      const list = covertSushiList(data)
+      setList(list)
+    }
+    fetchData()
+  }, [])
+
+  const covertSushiList = (data) => {
+    const array = []
+    for (const item of data) {
+      array.push({
+        id: item['catalogSectionUUID'],
+        title: item['payload.standardItemsPayload.catalogItems[0].title'],
+        image: item['payload.standardItemsPayload.catalogItems[0].imageUrl'],
+        subtitle: item['payload.standardItemsPayload.catalogItems[0].title'],
+        description:
+          item['payload.standardItemsPayload.catalogItems[0].itemDescription'],
+        price: item['payload.standardItemsPayload.catalogItems[0].price'],
+      })
+    }
+    return array
+  }
 
   return (
-    <div className="List">
-      <div className="row justify-content-between align-items-center">
-        <div className="col-lg-5 d-flex align-items-center">
-          <div className="col-lg-3">
-            <div className="btn-group">
-              <button id="sort-btn" type="button">
-                <i className="fa fa-sort-asc" aria-hidden="true"></i>Sort By
-                Price
-              </button>
-            </div>
-          </div>
-          <div className="d-flex align-items-center">
-            <button className="btn">Search</button>
-            <div className="input-group">
-              <input
-                type="text"
-                name="search"
-                id="search-field"
-                placeholder="enter Sushi name"
-                className="form-control"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="card-container cards-section1 mx-auto mt-4">
-        <h3 className="font-weight-bold mb-4">SUMOMAKI</h3>
+    <div className="container">
+      <div className="card-container cards-section1 mt-4">
+        <h3 className="font-weight-bold mb-4"></h3>
         <div id="item-container" className="row">
-          {list}
+          {list.map((item) => (
+            <SushiCard key={item.id} item={item} />
+          ))}
         </div>
       </div>
     </div>
   )
+}
+
+SushiList.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string,
+    })
+  ),
 }
 
 export default SushiList
